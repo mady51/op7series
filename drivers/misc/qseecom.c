2f1d7b74e20355c1b96edc61ba22e044afa867eb
Alex Naidis <alex.naidis@linux.com>
2016-11-09 12:20:54 +0100

    qseecom: micro optimization for module detection
    Applying a micro optimization which
    was suggested by Mike Chan:
    
    If we have a hardcoded string value,
    the sizeof macro which operates at compile
    time is preferred over the strlen function
    which operates at runtime.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -2845,6 +2845,9 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 		return -EINVAL;
 	}
 
+	pr_debug("unload app %d(%s), app_crash flag %d\n", data->client.app_id,
+			data->client.app_name, app_crash);
+
 	if (!memcmp(data->client.app_name, "keymaste", sizeof("keymaste")-1)) {
 		pr_debug("Do not unload keymaster app from tz\n");
 		goto unload_exit;

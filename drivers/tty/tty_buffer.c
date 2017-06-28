00724ec240dbd9311fae8c3c77d331a04a26cfab
Philip Cuadra <philipcuadra@google.com>
2017-06-28 12:45:08 -0700

    tty: move tty_port workqueue to be a kthread
    This makes each tty_port have their own kthread, hopefully allowing them
    a bit more freedom in scheduling when they reveive data.
    
    Based on a patch from Philip Cuadra <philipcuadra@google.com>
    Based on a patch from Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 36106419
    Test: run Bluetooth audio, ensure separate tty thread is created.
    Signed-off-by: Philip Cuadra <philipcuadra@google.com>
    Change-Id: I9fd25235b26a66acb37a40304c356209b74ad46c
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -71,7 +71,7 @@ void tty_buffer_unlock_exclusive(struct tty_port *port)
 	atomic_dec(&buf->priority);
 	mutex_unlock(&buf->lock);
 	if (restart)
-		kthread_queue_work(&port->worker, &buf->work);
+		queue_kthread_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL_GPL(tty_buffer_unlock_exclusive);
 
@@ -132,8 +132,7 @@ void tty_buffer_free_all(struct tty_port *port)
 	buf->tail = &buf->sentinel;
 
 	atomic_set(&buf->mem_used, 0);
-	if (!IS_ERR_OR_NULL(port->worker_thread))
-		kthread_stop(port->worker_thread);
+	kthread_stop(port->worker_thread);
 }
 
 /**
@@ -406,7 +405,7 @@ void tty_schedule_flip(struct tty_port *port)
 	 * flush_to_ldisc() sees buffer data.
 	 */
 	smp_store_release(&buf->tail->commit, buf->tail->used);
-	kthread_queue_work(&port->worker, &buf->work);
+	queue_kthread_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL(tty_schedule_flip);
 
@@ -573,8 +572,8 @@ void tty_buffer_init(struct tty_port *port)
 	atomic_set(&buf->mem_used, 0);
 	atomic_set(&buf->priority, 0);
 	buf->mem_limit = TTYB_DEFAULT_MEM_LIMIT;
-	kthread_init_work(&buf->work, flush_to_ldisc);
-	kthread_init_worker(&port->worker);
+	init_kthread_work(&buf->work, flush_to_ldisc);
+	init_kthread_worker(&port->worker);
 	port->worker_thread = kthread_run(kthread_worker_fn, &port->worker,
 					  "tty_worker_thread");
 	if (IS_ERR(port->worker_thread)) {
@@ -613,7 +612,7 @@ void tty_buffer_set_lock_subclass(struct tty_port *port)
 
 bool tty_buffer_restart_work(struct tty_port *port)
 {
-	return kthread_queue_work(&port->worker, &port->buf.work);
+	return queue_kthread_work(&port->worker, &port->buf.work);
 }
 
 bool tty_buffer_cancel_work(struct tty_port *port)
@@ -623,5 +622,5 @@ bool tty_buffer_cancel_work(struct tty_port *port)
 
 void tty_buffer_flush_work(struct tty_port *port)
 {
-	kthread_flush_work(&port->buf.work);
+	flush_kthread_work(&port->buf.work);
 }

35a94e905545d457974b405ad90a080ca945c09b
Danny Lin <danny@kdrag0n.dev>
2019-05-09 20:59:19 -0700

    drm/msm/sde: Omit debug logging code
    We're not going to debug the SDE driver in production. Don't compile the
    code at all to reduce the measurably significant overhead in frame commit
    hotpaths.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/gpu/drm/msm/sde/sde_kms.h
+++ drivers/gpu/drm/msm/sde/sde_kms.h
@@ -46,13 +46,7 @@
  * SDE_DEBUG - macro for kms/plane/crtc/encoder/connector logs
  * @fmt: Pointer to format string
  */
-#define SDE_DEBUG(fmt, ...)                                                \
-	do {                                                               \
-		if (unlikely(drm_debug & DRM_UT_KMS))                      \
-			DRM_DEBUG(fmt, ##__VA_ARGS__); \
-		else                                                       \
-			pr_debug(fmt, ##__VA_ARGS__);                      \
-	} while (0)
+#define SDE_DEBUG(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
 
 /**
  * SDE_INFO - macro for kms/plane/crtc/encoder/connector logs
@@ -70,13 +64,7 @@
  * SDE_DEBUG_DRIVER - macro for hardware driver logging
  * @fmt: Pointer to format string
  */
-#define SDE_DEBUG_DRIVER(fmt, ...)                                         \
-	do {                                                               \
-		if (unlikely(drm_debug & DRM_UT_DRIVER))                   \
-			DRM_ERROR(fmt, ##__VA_ARGS__); \
-		else                                                       \
-			pr_debug(fmt, ##__VA_ARGS__);                      \
-	} while (0)
+#define SDE_DEBUG_DRIVER(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
 
 #define SDE_ERROR(fmt, ...) pr_err("[sde error]" fmt, ##__VA_ARGS__)
 

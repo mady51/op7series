62f2a1d894c439ad97a3a84e8447fab7aee328b6
mady51 <root@instance-1.us-central1-a.c.reflected-coder-262904.internal>
2020-05-25 09:22:09 +0000

    Revert "thermal: devfreq_cooling: add new interface for direct power read"
    This reverts commit 96fae2150e95adfa70d7fd17a10f6fe2ec9d8d9a.

--- drivers/thermal/devfreq_cooling.c
+++ drivers/thermal/devfreq_cooling.c
@@ -359,11 +359,8 @@ static int devfreq_cooling_get_requested_power(struct thermal_cooling_device *cd
 		*power = dyn_power + static_power;
 	}
 
-		/* Scale dynamic power for utilization */
-		dyn_power *= status->busy_time;
-		dyn_power /= status->total_time;
-		/* Get static power */
-		static_power = get_static_power(dfc, freq);
+	trace_thermal_power_devfreq_get_power(cdev, status, freq, dyn_power,
+					      static_power, *power);
 
 	return 0;
 fail:

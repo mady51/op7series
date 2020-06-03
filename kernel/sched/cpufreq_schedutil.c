977e04eb82b5f471ff58f3bd62ed9ac86b52aa99
mady51 <root@instance-1.us-central1-a.c.reflected-coder-262904.internal>
2020-06-03 09:45:21 +0000

    tweak schedutil for custom

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -945,14 +945,17 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
+	tunables->up_rate_limit_us = 0;
+	//tunables->up_rate_limit_us =
+	//			cpufreq_policy_transition_delay_us(policy);
+	tunables->down_rate_limit_us = 0;
+	//tunables->down_rate_limit_us =
+	//			cpufreq_policy_transition_delay_us(policy);
+	tunables->hispeed_load = 90;
+	//tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 1209600;
 
-	tunables->iowait_boost_enable = false;
+	tunables->iowait_boost_enable = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
